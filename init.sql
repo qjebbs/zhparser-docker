63d54093b8f9585c24523067cac6ef3e8dce19e8
jebbs <qjebbs@gmail.com>
2019-01-16 11:21:29 +0800

    default_text_search_config chinese

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
 CREATE EXTENSION zhparser;
 CREATE TEXT SEARCH CONFIGURATION chinese (PARSER = zhparser);
 ALTER TEXT SEARCH CONFIGURATION chinese ADD MAPPING FOR n,v,a,i,e,l WITH simple;
+ALTER SYSTEM SET default_text_search_config = 'chinese';
